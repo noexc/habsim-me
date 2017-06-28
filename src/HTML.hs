129b93f58cf9918d4caddb3272bd0c18fb1cc00d
Ricky Elrod <ricky@elrod.me>
2017-06-28 07:23:48 -0400

    Try adding git commit SHA in html output using git hax
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/HTML.hs
+++ src/HTML.hs
@@ -33,7 +33,7 @@ navbar =  do
 template :: String -> Html () -> Html ()
 template title body =
   doctypehtml_ $ do
-    head_ $ do
+    head_ [makeAttribute "data-commit" "$Id$"] $ do
       meta_ [charset_ "utf-8"]
       meta_ [ name_ "viewport"
             , content_ "width=device-width, initial-scale=1, shrink-to-fit=no"
